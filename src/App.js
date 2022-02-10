18bb4ea187ef5ebbe01ed423faf832f692b29557
SiriusLL <sirius.love.light@gmail.com>
2022-02-09 20:28:59 -0700

    add html and css structure for todo list card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <div className="task-card">
+        <h2>Task Name</h2>
+        <p className="divider">
+          <span>List</span>
+        </p>
+        <form className="todo-form">
+          <div className="check-complete">
+            <p>X</p>
+            <p>placeholderplaceholder</p>
+            <input className="check-box" type="checkbox" />
+          </div>
+        </form>
+        <button>Add</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
