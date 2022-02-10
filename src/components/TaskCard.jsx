1d55832d1a949737fdf4295692ba07e6fce1996c
SiriusLL <sirius.love.light@gmail.com>
2022-02-10 00:27:38 -0700

    logic for adding tasks to list

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -3,8 +3,22 @@ import React, { useState } from "react";
 function TaskCard() {
   const [addTask, setAddTask] = useState(false);
 
+  const [newTask, setNewTask] = useState();
+
+  const [listData, setListData] = useState([]);
+
   const toggleTaskForm = () => {
     addTask ? setAddTask(false) : setAddTask(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(newTask);
+
+    setListData((prev) => [...prev, newTask]);
+    console.log(listData);
+    setNewTask("");
+    toggleTaskForm();
   };
 
   console.log(addTask);
@@ -17,16 +31,30 @@ function TaskCard() {
       </p>
       <form className="todo-form">
         <div className="check-complete">
-          <p>X</p>
-          <p>placeholderplaceholder</p>
-          <input className="check-box" type="checkbox" />
+          {listData &&
+            listData.map((task, i) => {
+              return (
+                <div className="task-row" key={i}>
+                  <p>X</p>
+                  <p className="task">{task}</p>
+                  <input className="check-box" type="checkbox" />
+                </div>
+              );
+            })}
         </div>
       </form>
-      {!addTask && <button onClick={toggleTaskForm}>+{`${addTask}`}</button>}
+      {!addTask && <button onClick={toggleTaskForm}>++</button>}
+      {newTask}
       {addTask && (
-        <form className="add-task-form">
-          <input type="text" placeholder="enter a task" />
-          <button onClick={toggleTaskForm}>Add Task</button>
+        <form className="add-task-form" onSubmit={(e) => handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="enter a task"
+            name="newTask"
+            onChange={(e) => setNewTask(e.target.value)}
+            value={newTask}
+          />
+          <button type="submit" value="submit" />
         </form>
       )}
     </div>
